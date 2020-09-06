27f1e4733b3cbe467558459ba7a2c451ae0208fe
Juan I Zumer <jizumer@gmail.com>
2020-09-07 00:27:37 +0200

    feat: Health check controller test.

--- apps/main/com/github/jizumer/rps/apps/backend/controller/health/HealthGetController.java
+++ apps/main/com/github/jizumer/rps/apps/backend/controller/health/HealthGetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-public class HealthCheckGetController {
+public class HealthGetController {
 
     @GetMapping("/health")
     public Map<String, String> index() {
