eac0bd0e864307735de912c95d45bd982f72f542
jizumer <jizumer@gmail.com>
2020-09-06 19:03:44 +0200

    config: Initial scaffolding

---
+++ src/test/java/com/github/jizumer/rps/RockPaperScissorsArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jizumer.rps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
