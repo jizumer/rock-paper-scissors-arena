ad72dc1af6d0b9c4d0a60cd8525313cf39cbcbac
jizumer <jizumer@gmail.com>
2020-09-10 22:28:04 +0200

    refactor: Non necessary package scan. This package is not necessary even in the boot-all mode (complete package scan from Boot class, instead of app-specific package scan), since all projects other than core, have explicit dependencies to core.

--- apps/main/com/github/jizumer/rps/apps/Boot.java
+++ apps/main/com/github/jizumer/rps/apps/Boot.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import java.util.Arrays;
 
 @SpringBootApplication
-@ComponentScan(value = {"com.github.jizumer.rps.core",
+@ComponentScan(value = {
         "com.github.jizumer.rps.playground",
         "com.github.jizumer.rps.dashboard",
         "com.github.jizumer.rps.apps"}
