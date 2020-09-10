b4fed399e9af783fa593b9deeee7f63a55738d32
jizumer <jizumer@gmail.com>
2020-09-10 22:51:41 +0200

    refactor: Removed unimplemented command processing.

--- apps/main/com/github/jizumer/rps/apps/frontend/RpsFrontendApplication.java
+++ apps/main/com/github/jizumer/rps/apps/frontend/RpsFrontendApplication.java
@@ -3,8 +3,6 @@ package com.github.jizumer.rps.apps.frontend;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.HashMap;
-
 @SpringBootApplication
 @ComponentScan(value = {
         "com.github.jizumer.rps.playground",
@@ -12,8 +10,5 @@ import java.util.HashMap;
         "com.github.jizumer.rps.apps.frontend"}
 )
 public class RpsFrontendApplication {
-    public static HashMap<String, Class<?>> commands() {
-        //TODO: Not yet defined
-        return new HashMap<>();
-    }
+
 }
